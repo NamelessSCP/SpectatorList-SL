391c1efc830da44ccf8e996f3c46cd7f95964427
Nameless <85962933+Misfiy@users.noreply.github.com>
2024-04-24 14:42:54 +0200

    yep

--- SpectatorList/SpectatorList.cs
+++ SpectatorList/SpectatorList.cs
@@ -1,24 +1,32 @@
-﻿using Exiled.API.Features;
+﻿using System;
+using Exiled.API.Features;
 
 namespace SpectatorList
 {
     public class SpectatorList : Plugin<Config>
     {
         public static SpectatorList Instance { get; private set; }
-        
+
         public override string Name { get; } = "Spectator List";
         public override string Author { get; } = "@misfiy";
+        public override Version Version { get; } = new Version(1,0,0);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 8, 1);
+
+        private EventHandler _handler;
 
         public override void OnEnabled()
         {
             Instance = this;
-            
+            _handler = new EventHandler();
+
             base.OnEnabled();
         }
 
         public override void OnDisabled()
         {
+            _handler = null;
             Instance = null;
+
             base.OnDisabled();
         }
     }
